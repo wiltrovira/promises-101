7d16dde127eaaa94b31d98420714e4298b380f45
Wilt Rovira <wrovira@gmail.com>
2017-09-03 02:09:13 -0500

    completado ejercicio de promises

---
+++ index2.js
@@ -0,0 +1,51 @@
+/* ES5 */
+/*Concatenar promesas - Llamar una promesa desde otra promesa*/
+var isMomHappy = true;
+
+// Promise
+var willIGetNewPhone = new Promise(
+    function (resolve, reject) {
+        if (isMomHappy) {
+            var phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone); // fulfilled
+        } else {
+            var reason = new Error('mom is not happy');
+            reject(reason); // reject
+        }
+
+    }
+);
+
+// 2nd promise
+var showOff = function (phone) {
+    return new Promise(
+        function (resolve, reject) {
+            var message = 'Hey friend, I have a new ' +
+                phone.color + ' ' + phone.brand + ' phone';
+
+            resolve(message);
+        }
+    );
+};
+
+// call our promise
+var askMom = function () {
+    console.log('before asking Mom'); // log before
+    willIGetNewPhone
+    .then(showOff) // chain it here
+    .then(function (fulfilled) {
+            console.log(fulfilled);
+         // output: 'Hey friend, I have a new black Samsung phone.'
+        })
+        .catch(function (error) {
+            // oops, mom don't buy it
+            console.log(error.message);
+         // output: 'mom is not happy'
+        });
+        console.log('after asking mom'); // log after
+};
+
+askMom();
---
+++ index3.js
@@ -0,0 +1,38 @@
+/* ES6 */
+/*Flat arrow*/
+const isMomHappy = false;
+
+// Promise
+const willIGetNewPhone = new Promise(
+    (resolve, reject) => { // fat arrow
+        if (isMomHappy) {
+            const phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone);
+        } else {
+            const reason = new Error('mom is not happy');
+            reject(reason);
+        }
+
+    }
+);
+
+const showOff = function (phone) {
+    const message = 'Hey friend, I have a new ' +
+                phone.color + ' ' + phone.brand + ' phone';
+    return Promise.resolve(message);
+};
+
+// call our promise
+const askMom = function () {
+    console.log('before asking Mom'); // log before
+    willIGetNewPhone
+        .then(showOff)
+        .then(fulfilled => console.log(fulfilled)) // fat arrow
+        .catch(error => console.log(error.message)); // fat arrow
+        console.log('after asking mom'); // log after
+};
+
+askMom();

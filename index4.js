27a6d2d91693d926ea1276e5f22438a20aae516b
Wilt Rovira <wrovira@gmail.com>
2017-09-03 02:35:25 -0500

    se agrega la versión ES7 con el archivo index4.js

---
+++ index4.js
@@ -0,0 +1,60 @@
+/* ES7 */
+const isMomHappy = true;
+
+// Promise
+const willIGetNewPhone = new Promise(
+    (resolve, reject) => {
+        if (isMomHappy) {
+            const phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone);
+        } else {
+            const reason = new Error('mom is not happy');
+            reject(reason);
+        }
+
+    }
+);
+
+// 2nd promise
+async function showOff(phone) {
+    return new Promise(
+        (resolve, reject) => {
+            var message = 'Hey friend, I have a new ' +
+                phone.color + ' ' + phone.brand + ' phone';
+
+            resolve(message);
+        }
+    );
+};
+
+// call our promise
+async function askMom() {
+    try {
+        console.log('before asking Mom');
+
+        let phone = await willIGetNewPhone;
+        let message = await showOff(phone);
+
+        console.log(message);
+        console.log('after asking mom');
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+}
+
+(async () => {
+    await askMom();
+})();
+
+
+/*
+Whenever you need to return a promise in a function, you prepend async to that function. 
+E.g. async function showOff(phone)
+Whenever you need to call a promise, you prepend with await. 
+E.g. let phone = await willIGetNewPhone; and let message = await showOff(phone);.
+Use try { ... } catch(error) { ... } to catch promise error, the rejected promise. 
+ */
